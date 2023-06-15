d31fd20d609cb92b63afdc4c90af0e6797fc551b
“Shravani-Jeurkar” <shravanimjeurkar@gmail.com>
2023-06-15 17:34:12 +0530

    Modified the about.php file, Added the quotes feature using API

--- about.php
+++ about.php
@@ -13,25 +13,32 @@
 
 <div class="container-fluid" id="main-content">
         <div class="row">
-            <div class="col-lg-11 m-auto p-4 overflow-hidden">
+            <div class="col-lg-10 m-auto p-4 overflow-hidden">
                 
                 <div class="card border-0 shadow-sm mb-4">
                     <div class="card-body">
                         <div class="text-center align-items-center justify-content-center mb-4">
                             <h3 class="mt-2 font1">About Us</h3>
-                            <p class="text-center mt-3 fs-5 ">
-                                Lorem ipsum dolor sit, amet consectetur adipisicing 
-                                elit. Ad veniam <br> tempora commodi ab fugit 
-                                molestiae amet quis nostrum totam exercitationem.
+                            <p class="text-center mt-3 fs-3 mx-auto al justify-content-between col-lg-6">
+                                Here, you can have a record of all the books which our library has, and can borrow any book by checking the availability of that book, in the books section.
+                                <br>Also, you can Search the desired book in the books section, by typing either any of the following:<br>
+                                <ul style="list-style:decimal; font-size:x-large; margin: 0 auto; width: 300px; ;" class="text-start">
+                                    <li>The Name of the book</li>
+                                    <li>Authors name</li>
+                                    <li>Edition</li>
+                                    <li>The Department name</li>
+                                </ul>
+                                <h3 class="mt-2">Hope you find your desired book in our library... <br>Happy reading <i class="bi bi-bookmark-heart-fill fs-2 "></i></h3>
                             </p>
+                            
                         </div>
-                        <div class="container">
+                        <!-- <div class="container">
                             <div class="row justify-content-between align-items-center">
                                 <div class="col-lg-6">
                                     
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
--- books.php
+++ books.php
@@ -1,3 +1,11 @@
+<?php
+  $login = 0;
+  if(isset($_SESSION['login']) && $_SESSION['login'] == true)
+  {
+      $login = 1;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,41 +19,150 @@
 <body class="bg-image" style="background-image: url('img/bg/2.webp'); overflow:initial !important;">
 <?php require('inc/header.php');?>
 
-<div class="container-fluid" id="main-content">
-        <div class="row">
-            <div class="col-lg-11 m-auto p-4 overflow-hidden">
-                
-                <div class="card border-0 shadow-sm mb-4">
-                    <div class="card-body">
-                        <div class="text-end d-flex align-items-center justify-content-center mb-4">
-                            <h3 class="mt-2 font1">Books</h3>
-                            <input type="text" oninput="search(this.value)" class="form-control shadow-none w-25 ms-auto" placeholder="Type to Search..">
-                        </div>
-                        <div class="table-responsive" style="min-height: 500px;">
-                            <table class="table table-hover border border-dark text-center" style="min-width: 1300px;">
-                                <thead>
-                                    <tr class="bg-dark text-light align-middle">
-                                        <th scope="col">#</th>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Author</th>
-                                        <th scope="col">Edition</th>
-                                        <th scope="col">Status</th>
-                                        <th scope="col">Quantity</th>
-                                        <th scope="col">Department</th>
-                                    </tr>
-                                </thead>
-                                <tbody id="books-data">
-                                    
-                                </tbody>
-                            </table>
+<?php
+    if(isset($_SESSION['login']) && $_SESSION['login'] == true)
+    {
+        echo<<<data
+        <div class="container-fluid" id="main-content">
+            <div class="row">
+                <div class="col-lg-11 m-auto p-4 overflow-hidden">
+                    
+                    <div class="card border-0 shadow-sm ">
+                        <div class="card-body">
+                            <div class="text-end d-flex align-items-center justify-content-center mb-4">
+                                <h3 class="mt-2 font1">Books</h3>
+                                <input type="text" oninput="search(this.value)" class="form-control shadow-none w-25 ms-auto" placeholder="Type to Search..">
+                            </div>
+                            <div class="table-responsive" style="min-height: 500px;">
+                                <table class="table table-hover border border-dark text-center" style="min-width: 1300px;">
+                                    <thead>
+                                        <tr class="bg-dark text-light align-middle">
+                                            <th scope="col">#</th>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Author</th>
+                                            <th scope="col">Edition</th>
+                                            <th scope="col">Status</th>
+                                            <th scope="col">Quantity</th>
+                                            <th scope="col">Department</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="books-data">
+                                        
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+data;
+    }
+    else
+    {
+        echo<<<data
+        <div class="container-fluid" id="main-content">
+            <div class="row">
+                <div class="col-lg-11 m-auto p-4 overflow-hidden">
+                    
+                    <div class="card border-0 shadow-sm ">
+                        <div class="card-body">
+                            <div class="text-center  align-items-center justify-content-center mb-4">
+                                <h3 class="mt-2 font1">Please Login</h3>
+                                <h3 class="font1">You have to log in to continue browsing the books...</h3>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+data;
+    }
+?>
+<div class="container-fluid" id="main-content">
+    <div class="row">
+        <div class="col-lg-11 m-auto p-4 overflow-hidden">
+            
+            <div class="card border-0 shadow-sm mb-4">
+                <div class="card-body">
+                    <div class="text-center  align-items-center justify-content-center mb-4">
+                        <h4 class="font1">Here are some Inspiring Quotes to make your day...</h4>
+                    </div>
+                    
+                    <div class="row justify-items-center output">
+                        <!-- <div class="card text-capitalize shadow-sm col-3 text-center mx-auto mb-3" >
+                            <div class="my-2 fs-4">
+                                happiness
+                            </div>
+                            <div class="card-body fs-3">
+                            <p>With supporting text below as a natural lead-in to additional content.</p>
+                            <h5 class="text-end">- Author Name</h5>
+                            </div>
+                        </div> -->
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
 <?php require('inc/footer.php');?>
 
 <script src="scripts/books.js"></script>
+<script>
+    var output = document.querySelector(".output");
+    let cat = ['happy', 'amazing', 'art', 'beauty'];
+    // var category = 'happiness';
+    function getQuotes()
+    {
+        output.innerHTML = 
+        `
+        <div class="text-center d-flex justify-content-center mt-2">
+            <div class="d-flex align-items-center">
+                <strong class="me-3">Loading...</strong>
+                <div class="spinner-border ml-auto" role="status" aria-hidden="true"></div>
+            </div>
+        </div>
+        `;
+        
+        const url = 'https://api.api-ninjas.com/v1/quotes?limit=3';
+        fetch(url,{
+            method: 'GET',
+            headers: { 'X-Api-Key': 'txaDAS+gCGadD07sLOt+ng==gWR4EaLNHcNOePXO'},
+            contentType: 'application/json',
+        }).then(function(res){
+            return res.text();
+        }).then(function(data){
+            display(JSON.parse(data));
+        }).catch(function(err){
+            output.innerHTML = err;
+        });
+
+        
+    }
+
+    function display(data){
+        output.innerHTML = "";
+        for (let i = 0; i < 3; i++)
+        {
+            var obj = data[i];
+            output.innerHTML +=
+            `
+            <div class="card shadow-sm col-lg-3 col-sm-7 col-md-7 text-center mx-auto mb-3">
+                <div class="fs-5 text-capitalize">${obj.category}</div>
+                <div class="card-body fs-4">
+                <p>${obj.quote}</p>
+                <h5 class="text-end">- ${obj.author}</h5>
+                </div>
+            </div>
+            `;
+
+            
+        }
+    }
+
+    getQuotes();
+</script>
 </body>
 </html>

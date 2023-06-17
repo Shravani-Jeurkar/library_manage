ab166cf52f7d314261e397a941a38a01e7e0ab9b
“Shravani-Jeurkar” <shravanimjeurkar@gmail.com>
2023-06-17 15:46:05 +0530

    Corrected some minor changes

--- admin/index.php
+++ admin/index.php
@@ -43,17 +43,26 @@
             height: 150px;
             width:150px;
             margin-left: 0rem!important;
+            margin-top: 0rem!important;
         }
 
         .con{
             margin-top: -20px !important;
+            margin-bottom: -60px;
         }
         
     }
 
+    .box{
+            height: 200px;
+            width: 200px;
+            margin-top: 10rem;
+
+        }
+
     .con{
         
-        margin-top: 120px;
+        margin-top: -50.5px;
         /* top: 50%;
         transform: translate(0%, -50%); */
     }
@@ -78,11 +87,11 @@
         {
             echo<<<data
             <div class="container con px-5 w-500 mx-auto">
-                <div class="row mx-lg-5 p-3 m-3 ">
+                <div class="row d-flex justify-content-center mx-lg-3 my-lg-5 p-3 m-3">
 
-                    <a href="books.php" class="col-3 btn p-2 ms-5 mb-5 box  rounded shadow" >
+                    <a href="books.php" class="col-3 btn p-2  me-lg-5 mb-5 box rounded shadow" >
                         <div class="container text-center align-items-center">
-                            <img src="../img/logo/pencil-square.svg" class="logo mt-5" height="100px">
+                            <img src="../img/logo/pencil-square.svg" class="logo mt-3" height="100px">
                             <h4 class="fw-bold mt-3">Edit Book</h4>
                         </div>
                     </a>
@@ -91,12 +100,13 @@
 
                     <a href="feedback.php" class="col-3 btn p-2 ms-5 box  rounded shadow">        
                         <div class="container text-center">
-                            <img src="../img/logo/card-text.svg" class="logo mt-5" height="100px">
+                            <img src="../img/logo/card-text.svg" class="logo mt-2" height="100px">
                             <h4 class="fw-bold mt-3 ">View Feedback</h4>
                         </div>
                     </a>
                 </div>
             </div>
+            <br><br><br><br>
 data;
         }
 
